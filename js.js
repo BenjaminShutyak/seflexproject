0923680f0278984e0c5f7a66f110ba9bd3dc2fbc
Benjamin Shutyak <bshutyak@gmail.com>
2022-08-28 20:45:16 -0600

    Working on JS

---
+++ js.js
@@ -0,0 +1,18 @@
+let weather = 
+    { apiKey: "098c215b3fefefbb89dabb3ae90080e8",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=imperial&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    }},
