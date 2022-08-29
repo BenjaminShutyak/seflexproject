8c70e2f6c89d49d760a32e9448751dee33a73e26
Benjamin Shutyak <bshutyak@gmail.com>
2022-08-28 20:47:11 -0600

    Function for weather data

--- js.js
+++ js.js
@@ -16,3 +16,13 @@ let weather =
         })
         .then((data) => this.displayWeather(data));
     }},
+
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =
+          "https://openweathermap.org/img/wn/" + icon + ".png";
+
